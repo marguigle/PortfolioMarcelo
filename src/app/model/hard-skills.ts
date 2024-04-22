8dd0a9bef9b7c2c940b7e52f1af43b97f0594b58
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-04-22 11:32:47 -0300

    se crea el modelo hard-skills

---
+++ src/app/model/hard-skills.ts
@@ -0,0 +1,10 @@
+export class HardSkills {
+  id?: number;
+  tecnologia: string;
+  porcentaje: string;
+
+  constructor(tecnologia: string, porcentaje: string) {
+    this.tecnologia = tecnologia;
+    this.porcentaje = porcentaje;
+  }
+}
