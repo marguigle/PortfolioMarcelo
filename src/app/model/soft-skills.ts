7e589c386ae024f54eaff39f1d2e51ae2c216a0e
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-04-22 11:39:35 -0300

    se crea model soft-skills

---
+++ src/app/model/soft-skills.ts
@@ -0,0 +1,9 @@
+export class SoftSkills {
+  id?: String;
+  habilidad: String;
+  porcentaje: String;
+  constructor(habilidad: String, porcentaje: String) {
+    this.habilidad = habilidad;
+    this.porcentaje = porcentaje;
+  }
+}
