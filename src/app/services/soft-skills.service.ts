0b36bf2df261c8a0bb126cb4991d06b8aa22fef8
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-04-23 19:56:15 -0300

    se hace la primera parte de softSkills

---
+++ src/app/services/soft-skills.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SoftSkills } from '../model/soft-skills';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class SoftSkillsService {
+  URL = 'http://localhost:8080/softskills/';
+  constructor(private http: HttpClient) {}
+  public getSoftSkills(): Observable<SoftSkills[]> {
+    return this.http.get<SoftSkills[]>(this.URL + 'traer');
+  }
+}
